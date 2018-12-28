6c4f92baec9a1b97f88abb662271a9494c0eeb06
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-28 20:16:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   INNER JOIN pledges
   ON users.id = pledges.user_id
   GROUP BY name
-  ORDER BY amount ASC, name ASC;"
+  ORDER BY sum(amount) ASC, name ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
